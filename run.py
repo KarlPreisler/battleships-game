cffdb5ab572f09916a7a2d5f64550d50eba46c3b
KarlPreisler <Karlpreisler@gmail.com>
2022-11-23 15:01:58 +0000

    create board and ask for players name, print players board

--- run.py
+++ run.py
@@ -1,113 +1,22 @@
-from random import randint
- 
-scores = {"computer": 0, "player": 0}
- 
+board = []
 
-class Board:
-    """
-    Main board class. set board size, the number of ships,
-    The players name and the board type (player board or computer board)
-    Has methods for adding ships and guesses and printing the board
-    """
- 
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
- 
-        # Initializes grid in one line
-        self.board = [["." for x in range(size)] for y in range(size)]
-       
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
- 
-        # Stores guesses against board.
-        self.guesses = []
-       
-        # Stores coordinates for ships on the board.
-        self.ships = []
-    
-    def print_board(self):
-        """ Method for printing board """
-        for row in self.board:
-            print("0".join(row))
- 
- 
-def guess(self, x, y):
-    self.guesses.append((x, y))
-    self.board[x][y] = "X"
-    # "X" Indicates guesses
- 
-    if (x, y) in self.ships:
-        self.board[x][y] = "*"
-        return "Hit"
-    else:
-        return "Miss"
-    # "*" indicates a hit
- 
-    def add_ships(self, x, y, type="computer"):
-        if len(self.ships) >= self.num_ships:
-            print("Error: your cannot add any more ships!")
-        else:
-            self.ships.append((x, y))
-            if self.type == "player":
-                self.board[x][y] = "@"
-                # "@" indicates player ships
- 
- 
-def random_point(size):
-    """
-    Helper function to return a random integer between 0 and size
-    returns random integer between 0-4.
-    """
-    return randint(0, size - 1)
- 
- 
-def populate_board(board):
-    """
-    Populates board for round
-    """
-    print(board.print_board())
-    
-    
-    
-   
-def play_game(computer_board, player_board):
-    print(f"{player_name}'s board\n")
-    x = print(int(input("Guess a row: ")))
-    y = print(int(input("Guess a column: ")))
- 
+for x in range(5):
+    board.append(["O"] * 5)
 
-def make_guess(board):
-    """
-    Processes the guesses
-    if its a computer guess it does same thing as populating board,
-    picking a random row and column for ships.
-    if player guess it promts for input
-    """
- 
 
-def new_game():
-    """
-    Starts a new game. Sets board size and number of ships, resets the
-    scores and initialises the boards.
-    """
-    size = 5
-    num_ships = 4
-    scores["computer"] = 0
-    scores["player"] = 0
-    print("-" * 35)
-    player_name = input("Please enter your name: ")
- 
-    computer_board = Board(size, num_ships, "Computer", type="computer")
-    player_board = Board(size, num_ships, player_name, type="player")
-    # Creates two class instances for computer & player
- 
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
-        # runs number of ships(4) which populates board for player and comp
-    
-    play_game(computer_board, player_board)
- 
+def print_board(board):
+    """ Function for printing computer and player board """
+    for row in board:
+        print(" ".join(row))
 
-new_game()
+
+def start_game():
+    """ Starts the game and gets player name """
+    print("Welcome to Battleships!")
+    player_name = str(input("Enter your name: "))
+
+    print(f"{player_name}'s board: ")
+
+
+start_game()
+print_board(board)
